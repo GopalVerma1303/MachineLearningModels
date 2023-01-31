d26c7bd76022c8f31bd5186c2e843014fcfb8498
Gopal Verma <vgopaldps@gmail.com>
2023-02-01 00:00:08 +0530

    till mid of the vidoe

--- Pandas_Practice/practice.py
+++ Pandas_Practice/practice.py
@@ -18,3 +18,14 @@ print(df.iloc[4:10])
 for index, rows in df.iterrows():
     print(index, rows['Name'])
 # %%
+print(df.loc[0])
+
+# %%
+print(df.describe())
+
+# %%
+print(df.sort_values(['Type 1', 'HP'], ascending=[1, 1])[['Type 1', 'HP']])
+
+# %%
+print(df.iloc[:, 4:10].sum(axis=1))
+# %%
