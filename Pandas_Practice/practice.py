50350140904d3f335cc419d555cc44b2935f5df3
Gopal Verma <vgopaldps@gmail.com>
2023-02-01 00:56:23 +0530

    end commit

--- Pandas_Practice/practice.py
+++ Pandas_Practice/practice.py
@@ -1,4 +1,5 @@
 # %%
+import re
 import pandas as pd
 df = pd.read_csv('pokemon_data.csv')
 print(df.head(3))
@@ -28,4 +29,12 @@ print(df.sort_values(['Type 1', 'HP'], ascending=[1, 1])[['Type 1', 'HP']])
 
 # %%
 print(df.iloc[:, 4:10].sum(axis=1))
+# %%
+new_df = df.loc[df['Name'].str.contains('Mega')]
+print(new_df)
+
+# %%
+new_df = df.loc[df['Type 1'].str.contains(
+    'fire|grass', flags=re.I, regex=True)]
+print(new_df)
 # %%
